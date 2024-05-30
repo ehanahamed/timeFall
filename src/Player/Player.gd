c080dc208a673a25340bead63205c68c5397c9af
mgamer1938 <127msafran@frhsd.com>
2024-05-30 09:30:37 -0400

    Yippie

--- src/Player/Player.gd
+++ src/Player/Player.gd
@@ -6,7 +6,7 @@ const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-@onready var anim = get_node("AnimatedSprite2D")
+@onready var anim = get_node("AnimationPlayer")
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -14,7 +14,7 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if (Input.is_action_just_pressed("ui_accept") and is_on_floor()) or (Input.is_action_just_pressed("ui_up") and is_on_floor()):
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
@@ -25,5 +25,12 @@ func _physics_process(delta):
 		anim.play("Run")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		anim.play("Idle")
+	
+	if direction < 0:
+		$AnimatedSprite2D.flip_h = true
+	else:
+		$AnimatedSprite2D.flip_h = false
+		
 
 	move_and_slide()
