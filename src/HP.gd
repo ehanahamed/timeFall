6a589ffae0ee3689639fe52c855037d10bde3c4e
mgamer1938 <127msafran@frhsd.com>
2024-06-04 09:24:31 -0400

    hjhtfh

--- src/HP.gd
+++ src/HP.gd
@@ -8,4 +8,4 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	text = "HP: " + str(get_node.($"../../Player").health)
+	text = "HP: " + str(get_node("../../Player/Player").health)
