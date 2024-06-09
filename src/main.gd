e2f26d4938dd3ce627904a452a576fdfaeedff2e
Ehan <ehanahamed@ehan.dev>
2024-06-09 02:14:57 -0400

    fix var type mismatch

--- src/main.gd
+++ src/main.gd
@@ -1,9 +1,6 @@
 extends Node2D
 
 func _ready():
-	if OS.has_feature("android") or OS.has_feature("web_android"):
-		Game.showTouchscreen = true
-
 	Utils.loadGame()
 	if Game.level == 0:
 		Game.level = 1
@@ -14,4 +11,4 @@ func _on_quit_pressed():
 
 
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://Level" + Game.level + ".tscn")
+	get_tree().change_scene_to_file("res://Level" + str(Game.level) + ".tscn")
