03b27fa871dab80783590fa0959525bcb2d0dd43
Ehan <ehanahamed@ehan.dev>
2024-06-09 15:03:11 -0400

    center logo on main menu

--- src/main.gd
+++ src/main.gd
@@ -7,5 +7,6 @@ func _on_play_pressed():
 	get_tree().change_scene_to_file("res://Level1.tscn")
 
 func _process(delta):
+	get_node("Logo").position.x = (get_viewport().get_visible_rect().size.x / 2)
 	get_node("Play").position.x = ((get_viewport().get_visible_rect().size.x / 2) - 200)
 	get_node("Quit").position.x = ((get_viewport().get_visible_rect().size.x / 2) - 200)
