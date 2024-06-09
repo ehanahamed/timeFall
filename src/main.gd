7fb4002b28f67513ba68eca6f416c2a9e6cfcfa8
Ehan <ehanahamed@ehan.dev>
2024-06-09 14:08:54 -0400

    remove saving and loading

--- src/main.gd
+++ src/main.gd
@@ -1,14 +1,7 @@
 extends Node2D
-
-func _ready():
-	Utils.loadGame()
-	if Game.level == 0:
-		Game.level = 1
-		Utils.saveGame()
 		
 func _on_quit_pressed():
 	get_tree().quit()
-
 
 func _on_play_pressed():
 	get_tree().change_scene_to_file("res://Level1.tscn")
