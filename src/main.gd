dd97d4bbddee534c46b8ccecbd52edb4518864a1
Ehan <ehanahamed@ehan.dev>
2024-06-09 14:29:27 -0400

    keep buttons center even on screen resize

--- src/main.gd
+++ src/main.gd
@@ -5,3 +5,7 @@ func _on_quit_pressed():
 
 func _on_play_pressed():
 	get_tree().change_scene_to_file("res://Level1.tscn")
+
+func _process(delta):
+	get_node("Play").position.x = ((get_viewport().get_visible_rect().size.x / 2) - 200)
+	get_node("Quit").position.x = ((get_viewport().get_visible_rect().size.x / 2) - 200)
